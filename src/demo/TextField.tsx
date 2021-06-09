83cd0f7c011e97bb8851e3ee8d556dea7cc803fe
VHM <taolaolataom@gmail.com>
2021-06-09 22:12:31 +0700

    UPDATE PACKAGE.json

--- src/demo/TextField.tsx
+++ src/demo/TextField.tsx
@@ -4,16 +4,16 @@ interface Props {
 	placeholder?: string;
 }
 
-const makeFullname = <
-	T extends { firstName?: string | "Vu"; lastName: string }
->(
-	obj: T
-) => {
-	return {
-		...obj,
-		fullname: obj.firstName + " " + obj.lastName
-	};
-};
+// const makeFullname = <
+// 	T extends { firstName?: string | "Vu"; lastName: string }
+// >(
+// 	obj: T
+// ) => {
+// 	return {
+// 		...obj,
+// 		fullname: obj.firstName + " " + obj.lastName
+// 	};
+// };
 
 const TextField = forwardRef<HTMLInputElement, Props>(
 	({ placeholder }, ref) => <input placeholder={placeholder} ref={ref} />
